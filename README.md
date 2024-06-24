936a264b17dd9b7a286434af1968897be83c7e5d
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2024-06-24 15:04:00 +0300

    copy the new updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# philo_task
+# flutter_application_2
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:philo_task/app/app.dart';
-import 'package:philo_task/core/helpers/firebase_options.dart';
+import 'firebase_options.dart';
+import 'ProductInfo.dart';
 
+// The main function is the starting point for all our Flutter Apps
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Future<void> _firebaseMessagingBackgroundHandler(
-  //     RemoteMessage message) async {
-  //   print("Handling a background message: ${message.messageId}");
-  // }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android
+  );
+  runApp(const MyApp());
+}
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  // await FirebaseMessaging.instance.requestPermission();
-
-  // Get FCM token
-  // String? token = await FirebaseMessaging.instance.getToken();
-  // print("FCM Token: $token");
-
-  runApp(MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProductInfo(),
+    );
+  }
 }
